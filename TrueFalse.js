f71639441fe45d746065b8045e91cc6d0b9cfd01
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-05 07:43:35 +0600

    True VS False

---
+++ TrueFalse.js
@@ -0,0 +1,21 @@
+//False
+// 0
+// ""
+//undefined
+//NaN
+//False
+
+//True
+//"0" " " []
+
+
+// const name = " ";
+
+let name;
+console.log(name);
+
+if(name){
+    console.log("Condition is True");
+}else{
+    console.log("Condition is False");
+}
