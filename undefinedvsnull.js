5ee10b6ad3bfa16cad96c533267ccb366c2ea87b
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-05 22:32:21 +0600

    This is double equal vs triple equal

--- undefinedvsnull.js
+++ undefinedvsnull.js
@@ -1,15 +1,21 @@
-let pakhi;
-console.log(pakhi);
+// let pakhi;
+// console.log(pakhi);
 
-const add = (num1,num2)=>{
-    num1 + num2;
-}
+// const add = (num1,num2)=>{
+//     num1 + num2;
+// }
 
-const object = {name:"Shakil", age:22};
-console.log(object.pakhi);
+// const object = {name:"Shakil", age:22};
+// console.log(object.pakhi);
 
-const fun = undefined;
-console.log(fun);
+// const fun = undefined;
+// console.log(fun);
 
-let ages = [50,60,70,10,20];
-console.log(ages[11]);
+// let ages = [50,60,70,10,20];
+// console.log(ages[11]);
+
+function doSomething(x, y){
+
+    console.log(y);
+    
+    }
