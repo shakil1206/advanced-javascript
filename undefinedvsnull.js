89e95d56a9f52b34d0d22d05f5b63cb41f1f5f8e
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-05 07:56:59 +0600

    Null vs undefined clear for interview

---
+++ undefinedvsnull.js
@@ -0,0 +1,15 @@
+let pakhi;
+console.log(pakhi);
+
+const add = (num1,num2)=>{
+    num1 + num2;
+}
+
+const object = {name:"Shakil", age:22};
+console.log(object.pakhi);
+
+const fun = undefined;
+console.log(fun);
+
+let ages = [50,60,70,10,20];
+console.log(ages[11]);
