8948b8513bcc161f35c65c1e9096cf66a6c930a3
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-06 10:15:45 +0600

    Scope practice end

---
+++ scope.js
@@ -0,0 +1,16 @@
+var a =50;
+function sum(num1, num2){
+    let result1 = num1 +num2;
+    
+        if(result1>10)
+        {
+            var mood = "happy";
+            console.log(mood);
+        }
+        console.log(mood);
+    return result1;   
+}
+
+
+const result = sum(10,20);
+console.log(result+a);
