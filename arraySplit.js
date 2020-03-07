93c6084cd2d48d4058bccfc1f3d90d7e55be8a7c
Md. Shakil Ahmed <shakilatrai5@gmail.com>
2020-03-07 07:50:55 +0600

    Array Split done

--- arraySplit.js
+++ arraySplit.js
@@ -12,3 +12,18 @@ function doSomething(x, y = 4){
     console.log(y);    
     }
     doSomething(3,2);
+
+
+if({}){
+    console.log("I am not false")
+}
+else{
+    console.log("I am false")
+}
+
+if( 'false' ){
+    console.log("I am not false")
+}
+else{
+    console.log("I am false")
+}
